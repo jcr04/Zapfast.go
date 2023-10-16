55b1bd1575f7ae4ba95b1e2c840876dcc7ccbb2d
jcr04 <joaocarlosreboucas15@gmail.com>
2023-10-16 20:10:17 -0300

    add features

---
+++ app/pkg/usecase/ride_usecase.go
@@ -0,0 +1,44 @@
+package usecase
+
+import (
+	"github.com/jcr04/Zapfast.go\app\pkg\domain"
+	"github.com/jcr04/Zapfast.go/pkg/repository"
+)
+
+
+type RideUsecase struct {
+	rideRepo repository.RideRepository
+}
+
+func NewRideUsecase(rr repository.RideRepository) *RideUsecase {
+	return &RideUsecase{rideRepo: rr}
+}
+
+func (ru *RideUsecase) RequestRide(customerID int, startLocation, endLocation string, price float64) (*domain.Ride, error) {
+	ride := &domain.Ride{
+		CustomerID:    customerID,
+		StartLocation: startLocation,
+		EndLocation:   endLocation,
+		Status:        domain.StatusRequested,
+		Price:         price,
+	}
+	return ru.rideRepo.Store(ride)
+}
+
+func (ru *RideUsecase) AcceptRide(rideID, driverID int) error {
+	ride, err := ru.rideRepo.FindByID(rideID)
+	if err != nil {
+		return err
+	}
+	ride.Accept(driverID)
+	return ru.rideRepo.Update(ride)
+}
+
+func (ru *RideUsecase) CompleteRide(rideID int) error {
+	ride, err := ru.rideRepo.FindByID(rideID)
+	if err != nil {
+		return err
+	}
+	ride.Complete()
+	return ru.rideRepo.Update(ride)
+}
